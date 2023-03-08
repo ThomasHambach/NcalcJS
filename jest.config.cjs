4a0d2342f6eb33724d89751c15ddd7040b51b6f2
Thomas Hambach <505642+ThomasHambach@users.noreply.github.com>
2023-03-08 13:03:34 +0800

    Move to have browser and node versions

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,17 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   coveragePathIgnorePatterns: ['<rootDir>/src/Grammar/', '<rootDir>/node_modules/'],
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!antlr4/.*)', '<rootDir>/src/Grammar/*.js'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx|js)?$': ['ts-jest'],
+    // '<rootDir>/node_modules/antlr4/*.ts': 'ts-jest',
     //'<rootDir>/src/Grammar/*.js': 'ts-jest',
   },
 };
